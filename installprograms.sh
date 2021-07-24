cb7dca2e7b65ab4135a84e6f91f54f62887c5b2b
Akın Kara <33626727+akinkara@users.noreply.github.com>
2021-07-24 14:15:20 +0300

    initial commands

---
+++ installprograms.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+echo "script started"
+
+# make updates
+
+dnf upgrade --refresh -y
+
+
+
+# enable the RPM Fusion  Free repository, use:
+
+dnf install \
+
+  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
+
+
+
+# Optionally, enable the RPM Fusion Nonfree repository:
+
+dnf install \
+
+  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+
+
+
+# Install fedy copr repository
+
+dnf copr enable kwizart/fedy -y
+
+
+
+# Install fedy
+
+dnf install fedy -y
+
+
+
+
+
+#bazı paketler içinde snapd gerekir 
+
+dnf install snapd -y 
+
+ln -s /var/lib/snapd/snap /snap
+
+
+
+echo "sleeping 20 seconds. because snap dont install packages immediately"
+
+sleep 20
+
+
+
+# brave, discord ve notepadqq kur
+
+snap install brave discord notepadqq 
+
+
+
+# vscode, pycharm pro ve datagrip kur
+
+snap install code --classic && snap install pycharm-professional --classic && snap install datagrip --classic
+
+
+
+#filezilla ve ekstra programları kur
+
+dnf install filezilla gnome-tweaks htop tlp dconf-editor -y
+
+
+
+echo "script completed successfully!"
