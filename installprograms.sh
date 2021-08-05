a69665c6877fa6445b3415cbf2371019cd872700
Akın Kara <33626727+akinkara@users.noreply.github.com>
2021-08-05 15:03:00 +0300

    update to snapd and add gnome customization
    snapd installations removed they will be installed manually, added gnome customizations

--- installprograms.sh
+++ installprograms.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
 # chmod +x installprograms.sh
-echo "script started"
+echo "Started!"
 
 # make updates
 
 dnf upgrade --refresh -y
-
-
+dnf update -y
 
 # enable the RPM Fusion  Free repository, use:
-
 dnf install \
-
   https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
-
-
-
 # Optionally, enable the RPM Fusion Nonfree repository:
-
 dnf install \
-
   https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-
 
 
 # Install fedy copr repository
@@ -29,46 +20,35 @@ dnf install \
 dnf copr enable kwizart/fedy -y
 
 
-
 # Install fedy
 
 dnf install fedy -y
 
 
-
-
-
-#bazı paketler içinde snapd gerekir 
-
-dnf install snapd -y 
-
-ln -s /var/lib/snapd/snap /snap
-
-
-
-echo "sleeping 20 seconds. because snap dont install packages immediately"
-
-sleep 20
-
-
-
-# brave, discord ve notepadqq kur
-
-snap install brave discord 
-
-
-
-# vscode, pycharm pro ve datagrip kur
-
-snap install code --classic && snap install pycharm-professional --classic && snap install datagrip --classic
-
-
-
-# ekstra programları kur
-
-dnf install filezilla gnome-tweaks htop dconf-editor nodejs postgresql postgresql-server power-profiles-daemon -y
+#ekstra programları kur
+dnf install -y filezilla gnome-tweaks htop dconf-editor nodejs postgresql postgresql-server power-profiles-daemon tlp vim pgadmin4 bibata-cursor-themes fedy neofetch arc-icon-theme
 
 postgresql-setup initdb                  # initialize PG cluster
 systemctl start postgresql               # start cluster
 
-echo "script completed successfully!"
+# arayüz tema, cursor vs ayarları yap
+
+# set gnome theme
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+
+# set cursor theme
+gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Ice'
+
+# set window button layout
+gsettings set org.gnome.desktop.wm.preferences button-layout 'close:' 
+
+# icon theme
+gsettings set org.gnome.desktop.interface icon-theme 'Arc'
+
+# middile click action
+gsettings set org.gnome.desktop.wm.preferences action-middle-click-titlebar 'minimize'
+
+# workspaces on every display
+gsettings set org.gnome.mutter workspaces-only-on-primary false
+
+echo "Completed Successfully!"
