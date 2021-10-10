a58a45d10ffafa83a327a5ab9de9760b39522be4
Akın Kara <33626727+akinkara@users.noreply.github.com>
2021-10-10 17:43:08 +0300

    w

--- installprograms.sh
+++ installprograms.sh
@@ -32,7 +32,9 @@ dnf copr -y enable user501254/Arc
 
 
 #ekstra programları kur
-dnf install -y filezilla gnome-tweaks htop dconf-editor nodejs postgresql postgresql-server power-profiles-daemon tlp vim bibata-cursor-themes fedy neofetch arc-icon-theme vlc xbindkeys pulseaudio-utils gnome-extensions-app gnome-shell-extension-dash-to-dock
+dnf install -y filezilla gnome-tweaks htop dconf-editor nodejs postgresql postgresql-server \ 
+power-profiles-daemon tlp vim bibata-cursor-themes fedy neofetch arc-icon-theme vlc xbindkeys \
+pulseaudio-utils gnome-extensions-app gnome-shell-extension-dash-to-dock screen cmatrix
 
 postgresql-setup initdb                  # initialize PG cluster
 systemctl start postgresql               # start cluster
