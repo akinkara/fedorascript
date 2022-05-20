84f9930ab33231f460be870cdc20f353d200f53b
Akın Kara <33626727+akinkara@users.noreply.github.com>
2022-05-20 21:46:21 +0300

    bugfix

--- updated_install.sh
+++ updated_install.sh
@@ -5,30 +5,30 @@ dnf upgrade --refresh -y
 dnf update -y
 
 # activating rpmfusion repos
-dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
 https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
 dkms make kernel-devel dnf-plugins-core \
 #kmod-wl # hp laptop wifi problemini bu driver çözdü
 
 # installing i3 and i3-related packages
 
-i3 i3status i3lock polybar rofi dmenu xbacklight feh arandr volumeicon lxpolkit picom \
+dnf install -y i3 i3status i3lock polybar rofi dmenu xbacklight feh arandr volumeicon lxpolkit picom \
 lxappearance lightdm-gtk-greeter-settings gtk-chtheme qt4-qtconfig \ 
 gnome-terminal nautilus \
 
 
 # general purpose stuff
 
-neofetch htop power-profiles-daemon tlp neovim vim catfish xkill xinput xprop mpv keepasxc libreoffice hwinfo git \
+dnf install -y neofetch htop power-profiles-daemon tlp neovim vim catfish xkill xinput xprop mpv keepasxc libreoffice hwinfo git
 
 # installing python and related packages
-python3-devel python3 python3.9 python3.8 python3-psycopg2 python3-pip 
+dnf install -y python3-devel python3 python3.9 python3.8 python3-psycopg2 python3-pip 
 python3.9 -m ensurepip
 python3.8 -m ensurepip
 
 # installing docker and docker related packages
 dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-dnf install docker-ce docker-ce-cli containerd.io docker-compose sen
+dnf install -y docker-ce docker-ce-cli containerd.io docker-compose sen
 systemctl enable docker
 systemctl start docker
 
@@ -43,7 +43,7 @@ dnf install brave-browser
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nena    bled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 check-update
-dnf install code
+dnf install -y code
 
 
 # check if you have ssd 
@@ -53,7 +53,7 @@ cat /sys/block/sda/queue/rotational
 
 # zoom installation
 wget https://zoom.us/client/latest/zoom_$(uname -m).rpm -O zoom.rpm
-dnf -y install ./zoom.rpm
+dnf install -y ./zoom.rpm
 rm zoom.rpm
 
 # teams installation
